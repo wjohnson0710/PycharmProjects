6a576655e2d8bfb4a6038c98955db3336cc1acfa
William Johnson <thejohnsons@Williams-MacBook-Pro.local>
2020-04-23 22:03:38 -0400

    Initial commit

---
+++ app.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+import random
+
+number = random.randint(1, 10)
+tries = 1
+
+
+uname = input(" Hello, What is your username? ")
+
+print("Hello,", uname + ".", )
+
+question = input("Would you like to play a game? [Y/N] ")
+if question == "n":
+    print("oh... okay")
+
+if question == "y":
+    print("I'm thinking of a number bewtween 1 & 10")
+    guess = int(input("Have a guess: "))
+    if guess > number:
+        print("Guess lower...")
+
+if guess < number:
+    print("Guess higher...")
+while guess != number:
+    tries += 1
+    guess = int(input("Try again: "))
+    if guess < number:
+        print("Guess higher")
+if guess == number:
+    print("You're right! You win! The number was", number, \
+          "and it only took", tries, "tries!")
